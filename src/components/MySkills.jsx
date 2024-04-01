e452f85d4e555bf71ba379d85d01d3f9b141ea5c
Carlos Eduardo Sabino de Lima <121118484+KaduLimaz@users.noreply.github.com>
2024-04-01 19:04:58 -0300

    Texto Perfil
    Texto Perfil

--- src/components/MySkills.jsx
+++ src/components/MySkills.jsx
@@ -32,7 +32,27 @@ export const MySkills = () => {
 						</SkillsCard>
 					))}
 				</SkillsCardContainer>
-				<div></div>
+
+				{/* --right section */}
+				<div>
+					<Heading as="h4" size="h4">
+						Minhas Skills
+					</Heading>
+					<Heading as="h2" size="h2" top="0.5rem">
+						Meu <BlueText>Objetivo</BlueText>
+					</Heading>
+
+					<ParaText top="2rem" bottom="1.5rem">
+						Estou buscando um Estágio em Programação onde possa aplicar minhas
+						habilidades em JavaScript e TypeScript para contribuir com o
+						desenvolvimento de software inovador e de alta qualidade. Possuo
+						conhecimento sólido de ambas as linguagens, incluindo seus
+						principais conceitos, frameworks e ferramentas. Sou um profissional
+						dedicado, proativo e com grande capacidade de aprendizado, além de
+						ter forte interesse em trabalhar em equipe e colaborar com outros
+						desenvolvedores.
+					</ParaText>
+				</div>
 			</FlexContainer>
 		</PaddingContainer>
 	);
